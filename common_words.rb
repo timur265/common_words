beaf52785745dfe91cbb62b72197736b013c5ab9
Timur Vorobyov <buskettim0902@gmail.com>
2020-05-13 12:06:03 +0500

    feature: Sorts hash

--- common_words.rb
+++ common_words.rb
@@ -1,14 +1,22 @@
 #!/usr/bin/env ruby
 
+def get_common_words_from_string(content)
+    h = {}
+    content.split.each {
+        |word|
+        if h.has_key?(word)
+            h[word] += 1
+        else
+            h[word] = 1
+        end
+    }
+    return Hash h 
+end
+
+
 file = File.open(ARGV[0])
 content = file.read
 file.close
-h = {}
-content.split.each {
-    |word|
-    if h.has_key?(word)
-        h[word] += 1
-    else
-        h[word] = 1
-    end
-}
+
+h = get_common_words_from_string(content)
+sorted_hash = h.sort_by{ |k, v| v}
