3de7742b428dd7a4a0eb5e05827dccc33ab10f35
Timur Vorobyov <buskettim0902@gmail.com>
2020-05-13 11:12:50 +0500

    feature: Fills a hash

--- common_words.rb
+++ common_words.rb
@@ -3,3 +3,12 @@
 file = File.open(ARGV[0])
 content = file.read
 file.close
+h = {}
+content.split.each {
+    |word|
+    if h.has_key?(word)
+        h[word] += 1
+    else
+        h[word] = 1
+    end
+}
